002c6d03a59004efa76e65fbfe54a14c94cccdf6
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-06-09 15:17:51 +0300

    Fix reportWebVitals tests

--- src/reportWebVitals.test.js
+++ src/reportWebVitals.test.js
@@ -1,25 +1,35 @@
 import reportWebVitals from './reportWebVitals';
 
+// Mock the web-vitals module
+jest.mock('web-vitals', () => ({
+  getCLS: jest.fn(),
+  getFID: jest.fn(),
+  getFCP: jest.fn(),
+  getLCP: jest.fn(),
+  getTTFB: jest.fn()
+}));
+
 describe('reportWebVitals', () => {
   test('should not call web-vitals when no callback is provided', () => {
-    const mockImport = jest.spyOn(global, 'import');
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = require('web-vitals');
     reportWebVitals();
-    expect(mockImport).not.toHaveBeenCalled();
+    expect(getCLS).not.toHaveBeenCalled();
+    expect(getFID).not.toHaveBeenCalled();
+    expect(getFCP).not.toHaveBeenCalled();
+    expect(getLCP).not.toHaveBeenCalled();
+    expect(getTTFB).not.toHaveBeenCalled();
   });
 
   test('should call web-vitals when callback is provided', () => {
     const mockCallback = jest.fn();
-    const mockImport = jest.spyOn(global, 'import').mockImplementation(() => 
-      Promise.resolve({
-        getCLS: jest.fn(),
-        getFID: jest.fn(),
-        getFCP: jest.fn(),
-        getLCP: jest.fn(),
-        getTTFB: jest.fn()
-      })
-    );
-
+    const { getCLS, getFID, getFCP, getLCP, getTTFB } = require('web-vitals');
+    
     reportWebVitals(mockCallback);
-    expect(mockImport).toHaveBeenCalledWith('web-vitals');
+    
+    expect(getCLS).toHaveBeenCalledWith(mockCallback);
+    expect(getFID).toHaveBeenCalledWith(mockCallback);
+    expect(getFCP).toHaveBeenCalledWith(mockCallback);
+    expect(getLCP).toHaveBeenCalledWith(mockCallback);
+    expect(getTTFB).toHaveBeenCalledWith(mockCallback);
   });
 }); 
